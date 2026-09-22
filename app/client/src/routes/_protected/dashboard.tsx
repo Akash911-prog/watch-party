7a9482f77c267cd757fcc6f52ca7af6a654479c4
Akash <Akashsamanta0571@gmail.com>
2026-09-22 13:31:58 +0530

    feat: realtime data in dashboard

--- app/client/src/routes/_protected/dashboard.tsx
+++ app/client/src/routes/_protected/dashboard.tsx
@@ -4,12 +4,24 @@ import {
   ActiveRooms,
   VideoLibrary,
 } from '@/components/dashboard';
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
+import { fetchVideos } from '@/lib/queryFunctions/videoFetch';
+
+const videosQueryOptions = queryOptions({
+  queryKey: ['video'],
+  queryFn: () => fetchVideos(),
+});
 
 export const Route = createFileRoute('/_protected/dashboard')({
+  loader: ({ context: { queryClient } }) => {
+    queryClient.ensureQueryData(videosQueryOptions);
+  },
   component: DashboardPage,
 });
 
 function DashboardPage() {
+  const { data: videos } = useSuspenseQuery(videosQueryOptions);
+
   return (
     <div className="dark min-h-screen w-full bg-background text-foreground">
       <div className="mx-auto px-6 py-8">
